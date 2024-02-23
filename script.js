95bdee7ce4aa9a45ce82694ce7d136692c9328bc
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-02-23 20:05:27 +0000

    added basics / if moused is pressed/ some of panda

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-/**
- * Project 1 - Interactive Image
- * Name: 
- * Comments: 
- */
+ /*
+    Your Name; Gracie Cohen
+    Completion date;
+    Basic description of the image; A panda (going toward bamboo?)
+    Basic description of user interactions to expect;
+    Citations of any code sources used for reference;
+    */
+
 
 // Global Variables go here
+// These variables will define the location of the panda
+var x; //horizontal location 
+var y = 100
+var d = 150 
+var bodyColor = 0; //black
+ 
 
 function setup(){
   // this function will run once
-  createCanvas(600, 400); // create a 600x400 pixel drawing canvas
-
+  createCanvas(800, 400); // create a 600x400 pixel drawing canvas
+  x = width / 5; // initial location
+  y = height / 2; 
+    // set x speed:
+    xSpeed = random (-4,4); // horizontal speed
 }
 
 function draw(){
   // this function runs again and again (60x per second)
-  background(200); //light gray background
+  background(0,0,180); // 
+  
+  fill ("blue"); // blue text
+  text ("click the mouse to move panda", 20, 50);  
   
   // add your image drawing code here
 
@@ -22,11 +37,24 @@ function draw(){
   Use the following if()...else() structure to incorporate mouse click control of your animation
 */
   if(mouseIsPressed){
-    //check each frame to see if the mouse is pressed, then do something
+    cheekColor = (128,0,0); // maroon
   } else {
-    // do something here if the mouse is NOT pressed
+    cheekColor = 255; // white
   }
-}
+
+  // Make panda
+
+  push(); // create new drawing layer
+  translate (x,y); // shift original point
+    // shapes are relative to (0,0) 
+
+  //Panda's head
+  fill(bodycolor);
+  ellipse (0, d, d)
+
+  //Panda's face 
+
+
 
 /** 
  * the mousePressed() function is separate from draw(). 
